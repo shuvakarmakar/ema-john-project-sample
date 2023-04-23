bc7b5a1b2610a6de357350cd68ddd54447b642f9
shuvakarmakar <shuva15-7953@diu.edu.bd>
2023-04-23 16:40:21 +0600

    Signup Component Added

---
+++ src/__steps__.js
@@ -0,0 +1,7 @@
+/**
+ * 1. Create Firebase Project
+ * 2. Create Web App
+ * 3. npm i firebase
+ * 4. save firebase config and export default app
+ * 5. Build > Authetication > enable sign in method
+ */
